364e65d89c282e1799f258ea7436e10e4b1d2d96
Capable <capableapps@icloud.com>
2022-12-27 19:46:15 +0530

    todo router commited

--- Backend/TodoRouter.ts
+++ Backend/TodoRouter.ts
@@ -1,4 +1,5 @@
 import express, {Router, Express} from "express";
+import bodyParser from "body-parser";
 
 class Todo {
     todoKey: number;
@@ -23,14 +24,38 @@ class TodoRouter {
     }
 
     private addRoutes(): void {
-        this.router.get("/", (req, res)=> {
-            console.log(req);
-            res.send({"todoKey":"1", "todoItem":'Buy Milk'});
-        });
+      this.router.get("/", (_,res)=>res.send(this.todoList));
 
-        this.router.post("/", (req, res)=> {
-            res.send("kya hua be request");
-        })
+      this.router.get("/:tk", (req, res) => {
+        const key = +req.params.tk;
+        console.log("parameter",key);
+
+        const foundTodo = this.todoList.find( todo => todo.todoKey == key);
+
+        if (foundTodo)
+            res.send(foundTodo);
+        else
+            res.sendStatus(404);
+      });
+
+      this.router.post("/", (req,res) => {
+        const body = req.body;
+        let lastKey = (this.todoList?.at(-1)?.todoKey)! + 1;
+        const todo = new Todo(lastKey ?? 1, req.body.todoItem);
+        this.todoList.push(todo);
+        res.send(todo);
+      });
+
+      this.router.delete("/:todoKey", (req,res) => {
+            const found = this.todoList.find(todo => todo.todoKey == +req.params.todoKey);
+
+            if (!found)
+                res.sendStatus(404);
+            else {
+                this.todoList = this.todoList.filter(todo => todo.todoKey != +req.params.todoKey);
+                res.send(`Todo ${req.params.todoKey} deleted`);
+            }
+      });
     }
 }
 
